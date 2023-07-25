fd870536e2a46ea37556906056213e96af402d9a
Muhammad Rizqi Ramadhan <139779066+Rizqi78800@users.noreply.github.com>
2023-07-25 21:06:13 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $hostname="localhost";
-    $username="root";
-    $password="";
-    $database="ranks_class";
-
-    $conn= mysqli_connect($hostname, $username, $password, $database);
-
-?>
