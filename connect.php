a5260ee32aeb794cd05f1cd2b427c1341bc1c573
Muhammad Rizqi Ramadhan <139779066+Rizqi78800@users.noreply.github.com>
2023-07-25 20:57:12 +0700

    Add files via upload
    Menambahkan input ke database

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+    $hostname="localhost";
+    $username="root";
+    $password="";
+    $database="ranks_class";
+
+    $conn= mysqli_connect($hostname, $username, $password, $database);
+
+?>
